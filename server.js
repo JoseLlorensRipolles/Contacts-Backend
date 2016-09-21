aeb52d80bab4ac4dafa11e17057fdef4cc7e8eeb
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-09-21 09:21:31 +0200

    Created suport for retrieving full contact list in a JSON format

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 var MongoClient = require('mongodb').MongoClient;
+var Db = require('mongodb').Db;
 var assert = require('assert');
 
 var express = require('express');
 var app = express();
 
+varJSON = require('JSON');
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support for encoded bodies
 
 var url = 'mongodb://localhost:27017/test';
 
-app.get('/',function(req,res){
-	res.send('Hello World');
+app.get('/api/contacts',function(req,res){
+	MongoClient.connect(url,function(err,db){
+		if(!err){
+			db.collection('contacts').find().toArray(function(err,docs){
+				if(!err){
+					res.setHeader('Content-type','application/json');
+					//res.send(JSON.stringify(docs));
+					res.send(docs);
+					console.log("Contact sended correctly")
+				}
+			})
+		}
+	})
 });
 
-// POST http://localhost:8080/api/users
-// parameters sent with 
+
 app.post('/api/addContact', function(req, res) {
     console.log(req.body.name + " -> " + req.body.phone);
     MongoClient.connect(url,function(err,db){
@@ -27,7 +40,7 @@ app.post('/api/addContact', function(req, res) {
     		});
     	}
     })
-    res.send(req.body.name);
+    res.send(req.body.name + "Saved correctly");
     db.close();
 });
 
